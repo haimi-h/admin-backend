843766fb98afe13270dadac7a4c302dfe78e31b3
Haimanot Hailu <haimih292@gmail.com>
2025-07-16 00:52:37 -0900

    bug fix

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,6 +8,7 @@ import axios from "axios";
 // const API = 'http://localhost:5000/api/auth';
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api';
 
+
 function Login() {
   const navigate = useNavigate();
 
